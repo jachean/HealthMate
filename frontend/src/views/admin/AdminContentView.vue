82eaed437c29b603fd659ea3f4a2647f34f29be7
Jachean Mihai <j234mediplant@gmail.com>
2026-03-06 19:24:23 +0200

    feat: add admin modules and clinic-scoped admin role

--- frontend/src/views/admin/AdminContentView.vue
+++ frontend/src/views/admin/AdminContentView.vue
@@ -39,7 +39,7 @@ function slugify(text) {
   return text.toLowerCase()
     .normalize('NFD').replace(/[\u0300-\u036f]/g, '')
     .replace(/[^a-z0-9]+/g, '-')
-    .replace(/^-+|-+$/g, '')
+    .replace(/^-+/, '').replace(/-+$/, '')
 }
 
 function onServiceNameInput(val) {
